2b1e107edbcfbfcb4e9922bdcd0085cf01e8ac25
Jinnabelle Garce <jinnabelle.garce@gmail.com>
2022-07-24 22:47:03 -0700

    updated tables

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,8 +4,8 @@ mechacar_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground
 summary(mechacar_lm) # Use summary() function to determine the p-value and r-squared value
 
 suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) #import and read suspension_coil.csv
-total_summary <- suspension_table %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create total_summary dataframe to get mean, median, variance and stdev of PSI
-lot_summary <-suspension_table %>% group_by(Manufacturing_Lot)%>%  summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create lot_summary dataframe and use groupby to get mean, median, variance and stdev of PSI
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI),Median=median(PSI), Var=var(PSI), Std_Dev=sd(PSI), Num_Coil=n(), .groups = 'keep') 
+lot_summary <- suspension_table  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep')#create lot_summary dataframe and use groupby to get mean, median, variance and stdev of PSI
 
 t.test(suspension_table$PSI,mu=1500)
 lot1_test <- subset(suspension_table, Manufacturing_Lot=="Lot1")
